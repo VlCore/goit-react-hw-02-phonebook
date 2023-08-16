8bb93a90615dab95528e346698d4e0c14facda50
VlCore <korovin_vladislav@icloud.com>
2023-08-16 18:50:56 +0300

    ff

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,29 +1,24 @@
-import { Contact } from "./Contact/Contact"
+import { Contact } from "./Contact/Contact";
 import PropTypes from 'prop-types';
 import { List } from "./ContactsList.styled";
 
-export const ContactsList = ({ contacts = [], filter, removeContact }) => {
-
-    const filteredCotacts = contacts.filter(el => el.name.toLowerCase().includes(filter.toLowerCase()))
-
-    return <List>
-        {contacts.length === 0 
-            ? <li><h3>Sorry, but the list is empty!</h3></li>
-            :filter
-                    ? filteredCotacts.map(contact => <Contact contact={contact} removeContact={removeContact} key={contact.id}/>)
-                    :contacts.map(contact => <Contact contact={contact} removeContact={removeContact} key={contact.id}/>)} 
-    </List>
-}
-
+export const ContactsList = ({ contacts = [], removeContact }) => {
+    return (
+        <List>
+            {contacts.length === 0 
+                ? <li><h3>Sorry, but the list is empty!</h3></li>
+                : contacts.map(contact => <Contact contact={contact} removeContact={removeContact} key={contact.id}/>)}
+        </List>
+    );
+};
 
 ContactsList.propTypes = {
-    filter: PropTypes.string,
     contacts: PropTypes.arrayOf(
         PropTypes.shape({
             number: PropTypes.string,
             name: PropTypes.string,
             id: PropTypes.string,
-        }),
+        })
     ),
-    removeContact:PropTypes.func.isRequired,
+    removeContact: PropTypes.func.isRequired,
 };
